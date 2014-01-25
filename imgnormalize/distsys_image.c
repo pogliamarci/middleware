a61bec9c18c319d8c5bdfb52caa82e381976bf58
Alecsh <holahula.go@gmail.com>
2014-01-25 13:51:16 +0100

    Image library updated
    read implemented but not tested

--- imgnormalize/distsys_image.c
+++ imgnormalize/distsys_image.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -5,14 +6,76 @@
 
 int image_read(const char* path, image_t** image)
 {
-    //TODO
-    return -1;
+	FILE* fp;
+	BITMAPFILEHEADER bitmapFileHeader;
+	BITMAPINFOHEADER bitmapInfoHeader;
+
+	fp = fopen(path, "rb");
+	
+	if(fp == NULL)
+		return -1;
+
+	fread(&bitmapFileHeader, sizeof(BITMAPFILEHEADER), 1, fp);
+
+	//verify that this is a bmp file by check bitmap id
+    	if (bitmapFileHeader.bfType != 0x4D42)
+    	{
+        	fclose(fp);
+        	return -1;
+    	}
+
+	// read the bitmap info header
+	fread(&bitmapInfoHeader, sizeof(BITMAPINFOHEADER), 1, fp);
+
+	(*image)->header.width = bitmapInfoHeader.biWidth;
+	(*image)->header.height = bitmapInfoHeader.biHeight;
+	(*image)->header.channels = 1; // bitmapInfoHeader.boh;
+
+	// move file point to the begging of bitmap data
+	fseek(fp, bitmapFileHeader.bfOffBits, SEEK_SET);
+
+	*image = (image_t*) malloc(sizeof(image_t));
+
+	(*image)->data = (uint8_t*) malloc(image_num_pixels((*image)->header) * sizeof(uint8_t));
+
+	// verify memory allocation
+    	if (!(*image)->data)
+	{
+        	free((*image)->data);
+        	fclose(fp);
+        	return -1;
+	}
+
+	fread((*image)->data, sizeof(uint8_t), image_num_pixels((*image)->header), fp);
+
+	// make sure bitmap image data was read
+    	if ((*image)->data == NULL)
+    	{
+        	fclose(fp);
+        	return -1;
+    	}
+
+	fclose(fp);
+
+	return 1;
 }
 
 int image_write(const char* path, image_t image)
 {
-    //TODO
-    return -1;
+	FILE* fp;
+
+        fp = fopen(path, "wb");
+
+        if(fp == NULL)
+                return -1;
+
+	//fwrite();
+
+	fwrite(image.data, sizeof(uint8_t), image_num_pixels(image.header), fp);
+
+        fclose(fp);
+
+        return 1;
 }
 
 void image_free(image_t* image)
@@ -22,8 +85,7 @@ void image_free(image_t* image)
     free(image);
 }
 
-int image_num_pixels(const img_header_t* header)
+int image_num_pixels(const img_header_t header)
 {
-    //TODO
-    return header->channels * header->width * header->height;
+    return header.channels * header.width * header.height;
 }
--- imgnormalize/distsys_image.h
+++ imgnormalize/distsys_image.h
@@ -5,7 +5,34 @@
 #include <stdlib.h>
 
 
+
 /****** Data types *******/
+
+typedef struct
+{
+	uint16_t bfType;	// specifies the file type
+	uint32_t bfSize;	// specifies the size in bytes of the bitmap file
+	uint16_t bfReserved1;	// reserved; must be 0
+	uint16_t bfReserved2;	// reserved; must be 0
+	uint32_t bfOffBits;	// species the offset in bytes from the bitmapfileheader to the bitmap bits
+}BITMAPFILEHEADER;
+
+
+typedef struct
+{
+	uint32_t biSize;		// specifies the number of bytes required by the struct
+	uint64_t biWidth;		// specifies width in pixels
+	uint64_t biHeight;		// species height in pixels
+	uint16_t biPlanes;		// specifies the number of color planes, must be 1
+	uint16_t biBitCount;		// specifies the number of bit per pixel
+	uint32_t biCompression;		// spcifies the type of compression
+	uint32_t biSizeImage;		// size of image in bytes
+	uint64_t biXPelsPerMeter;	// number of pixels per meter in x axis
+	uint64_t biYPelsPerMeter;	// number of pixels per meter in y axis
+	uint32_t biClrUsed;		// number of colors used by the bitmap
+	uint32_t biClrImportant;	// number of colors that are important
+}BITMAPINFOHEADER;
+
 
 typedef struct {
 	size_t width;
@@ -18,6 +45,7 @@ typedef struct {
 	img_header_t header;
 	uint8_t* data;
 }image_t;
+
 
 
 /****** Functions ******/
@@ -38,6 +66,6 @@ int image_write(const char* path, image_t image);
 
 void image_free(image_t* image);
 
-int image_num_pixels(const img_header_t* header);
+int image_num_pixels(const img_header_t header);
 
 #endif
