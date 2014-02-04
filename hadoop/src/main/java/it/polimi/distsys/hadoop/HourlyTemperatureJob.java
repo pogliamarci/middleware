0dd3ccac96636fc8312e436e6c936ee6f7aef0b8
Marcello Pogliani <pogliamarci@hotmail.it>
2014-02-04 10:21:37 +0100

    Read first line of the file

--- hadoop/src/main/java/it/polimi/distsys/hadoop/HourlyTemperatureJob.java
+++ hadoop/src/main/java/it/polimi/distsys/hadoop/HourlyTemperatureJob.java
@@ -1,8 +1,13 @@
 package it.polimi.distsys.hadoop;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
 
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.mapreduce.Job;
@@ -14,7 +19,7 @@ import org.apache.hadoop.util.ToolRunner;
 public class HourlyTemperatureJob extends Configured implements Tool {
 	
 	@Override
-	public int run(String[] args) {
+	public int run(String[] args) throws URISyntaxException {
 		
 		if(args.length != 2) {
 			System.err.println("Usage: [in] [out]");
@@ -27,10 +32,20 @@ public class HourlyTemperatureJob extends Configured implements Tool {
 			job.setJarByClass(getClass());
 			FileInputFormat.addInputPath(job, new Path(args[0]));
 			FileOutputFormat.setOutputPath(job, new Path(args[1]));
+			
+			FileSystem fs = FileSystem.get(getConf());
+			InputStream input = fs.open(new Path(args[0]));
+			BufferedReader br = new BufferedReader(new InputStreamReader(input));
+			String initialTimestamp = br.readLine().split(",")[1].trim();
+			fs.close();
+			
+			getConf().set("initialTimestamp", initialTimestamp);
+			
 			job.setMapperClass(HourlyTemperatureMapper.class);
 			job.setReducerClass(HourlyTemperatureReducer.class);
 			job.setOutputKeyClass(HourlyTemperatureKey.class);
 			job.setOutputValueClass(DoubleWritable.class);
+			
 			return job.waitForCompletion(true) ? 0 : 1;
 		} catch (IOException e) {
 			System.err.println("Error: can't run the job: I/O error");
--- hadoop/src/main/java/it/polimi/distsys/hadoop/HourlyTemperatureMapper.java
+++ hadoop/src/main/java/it/polimi/distsys/hadoop/HourlyTemperatureMapper.java
@@ -9,22 +9,29 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class HourlyTemperatureMapper extends Mapper<LongWritable, Text, HourlyTemperatureKey, DoubleWritable> {
 
-	public static final long firstTimestamp = 0; // TODO TODO TODO
+	private long firstTimestamp = 0;
+	private static final long WINDOW_LENGTH = 3600;
 	
 	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		super.setup(context);
+		firstTimestamp = Long.parseLong(context.getConfiguration().get("initialTimestamp"));
+	}
+
+	@Override
 	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-		
-		//TODO hadoop does guarantee to break on newline, right?
+
 		String line = value.toString();
 		String[] tokens = line.split(",");
-		if(tokens.length != 5) // sanity check
+		if(tokens.length != 6) // sanity check
 			return;
 		
-		int hour = timestampToHour(Long.parseLong(tokens[0]));
-		int house = Integer.parseInt(tokens[1]);
-		int household = Integer.parseInt(tokens[2]);
-		int plug = Integer.parseInt(tokens[3]);
-		double meas = Double.parseDouble(tokens[4]);
+		int hour = timestampToHour(Long.parseLong(tokens[1]));
+		int house = Integer.parseInt(tokens[2]);
+		int household = Integer.parseInt(tokens[3]);
+		int plug = Integer.parseInt(tokens[4]);
+		double meas = Double.parseDouble(tokens[5]);
 		
 		context.write(new HourlyTemperatureKey(hour, house, household, plug),
 				new DoubleWritable(meas));
@@ -32,7 +39,6 @@ public class HourlyTemperatureMapper extends Mapper<LongWritable, Text, HourlyTe
 	}
 	
 	private int timestampToHour(long timestamp) {
-		final long WINDOW_LENGTH = 3600;
 		return (int) ((timestamp - firstTimestamp) % WINDOW_LENGTH);
 	}
 	
