74b9c76dfbef1f6605987639b9c246f7ac235bf7
Marcello Pogliani <pogliamarci@hotmail.it>
2014-02-06 18:08:39 +0100

    Stub map reduce

--- hadoop/src/main/java/it/polimi/distsys/hadoop/TemperatureOutliersReducer.java
+++ hadoop/src/main/java/it/polimi/distsys/hadoop/TemperatureOutliersReducer.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -15,20 +16,47 @@ public class TemperatureOutliersReducer extends Reducer<TemperatureOutliersKey, 
 	@Override
 	protected void reduce(TemperatureOutliersKey key, Iterable<TemperatureOutliersValue> values, Context context) throws IOException, InterruptedException {
 		
-		// compute the "general" median
-		// divide the iterator for each plug
-		// and compute the median of the plug
-		// TODO check if we can exploit Hadoop to do some sorting and simplify the processing in the reducer!
 		
+		int median = computeMedian(values);
+		
+		Map<Integer, List<Integer>> mp = new HashMap<Integer, List<Integer>>();
+		for(TemperatureOutliersValue v : values)
+		{
+			if(mp.get(v.getPlug()) != null) {
+				mp.put(v.getPlug(), new ArrayList<Integer>());
+			}
+			mp.get(v.getPlug()).add(v.getMeasurement());
+		}
+		
+		int tn = 0, p = 0;
+		for(Entry<Integer, List<Integer>> e : mp.entrySet())
+		{
+			tn++;
+			int m = computeMedian(e.getValue());
+			if(m > median)
+				p += 1;
+		}
+		context.write(key, new DoubleWritable(((double) tn / (double) p)));
 	}
 	
-	private double computeMedian(List<Double> lst) {
-		if(lst.size() == 0)
+	private int computeMedian(Iterable<TemperatureOutliersValue> values) 
+	{
+		List<Integer> loi = new ArrayList<Integer>();
+		for(TemperatureOutliersValue v : values)
+		{
+			loi.add(v.getMeasurement());
+		}
+		return computeMedian(loi);
+	}
+	
+	private int computeMedian(List<Integer> loi) {
+		Collections.sort(loi);
+		if(loi.size() == 0)
 			throw new IllegalArgumentException();
-		if(lst.size()%2 == 0) {
-			return (lst.get(lst.size()/2) + lst.get(lst.size()/2 -1)) / 2;
+		if(loi.size()%2 == 0) {
+			return (loi.get(loi.size()/2) + loi.get(loi.size()/2 -1)) / 2;
 		} else {
-			return lst.get(lst.size()/2);
+			return loi.get(loi.size()/2);
 		}
 	}
 
