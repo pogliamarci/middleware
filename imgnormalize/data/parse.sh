49e08c79738198a38a78f36f779c6c54a111e360
Marcello Pogliani <pogliamarci@hotmail.it>
2014-02-14 00:36:48 +0100

    [imgnormalize/data] Folder cleanup

--- imgnormalize/data/parse.sh
+++ imgnormalize/data/parse.sh
@@ -1,4 +1,6 @@
-for f in *.txt;
+directory=$1
+
+for f in $directory/*.txt;
 do
     cat $f | grep elapsed | cut -d ' ' -f 3 | sed s/elapsed//g | sed s/:/./g | awk -F. '{print ($1 * 6000) + ($2 * 100) + $3 }' > ${f}.parsed
 done
