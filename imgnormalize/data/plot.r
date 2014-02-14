00ed6c1cce7b8de3d09f48b8f2ca0c26a93765f7
Marcello Pogliani <pogliamarci@hotmail.it>
2014-02-14 01:38:35 +0100

    [imgnormalize/data] New plots + bugfixes
    I have run all the tests again to see if there were some glitches that
    caused the strange behavior in the earth-huge test. The results are
    actually the same as before, so we should investigate the code to see
    why in the earth-huge case MPI is consistently (slightly) better than
    OpenMP.
    
    Anyway, I substituted all the data and the plots with the new ones.

--- imgnormalize/data/plot.r
+++ imgnormalize/data/plot.r
@@ -27,16 +27,16 @@ prepare_scale_y <- function(vec, step, breakOnAll) {
 for(type in types) {
     for(f in points) {
         read <- read.table(file=paste(c('runs_',type, "_", f, "thread.txt.parsed"),collapse=""))
-        rbind(df, data.frame(x=f, y=read$V1, t=type)) -> df
+        rbind(df, data.frame(x=f, y=read$V1/100, t=type)) -> df
     }
 }
 
 p <- ggplot(data=df, aes(x=factor(x),y=y,color=t)) + stat_summary(fun.y="mean", geom="point", size=5)
 p = p + stat_summary(fun.ymax = errorUpper, fun.ymin = errorLower, geom = "errorbar", size=0.5, width=.15)
-p = p + xlab("Number of threads (OpenMP threads or MPI processes)") + ylab("Execution time [ms]")
+p = p + xlab("Number of threads (OpenMP threads or MPI processes)") + ylab("Execution time [s]")
 p = p + scale_color_brewer(palette="Set1")
 p = p + scale_fill_brewer(palette="Set1")
-p = p + prepare_scale_y(df$y, 10, FALSE)
+p = p + prepare_scale_y(df$y, 0.1, FALSE)
 p = p + theme(axis.text=element_text(colour="black"))
 p = p + theme(legend.title=element_blank())
 p = p + theme(axis.title.x=element_text(size=16)) + theme(axis.title.y=element_text(size=16))
