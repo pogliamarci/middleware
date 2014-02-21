e40bf0c55bf915badf7ec8d76885abfdba53ddd1
Marcello Pogliani <pogliamarci@hotmail.it>
2014-02-21 11:10:50 +0100

    [jmscluster] hadoop

--- jmscluster/src/main/java/it/polimi/distsys/jmscluster/utils/LocalAdmin.java
+++ jmscluster/src/main/java/it/polimi/distsys/jmscluster/utils/LocalAdmin.java
@@ -1,14 +1,23 @@
 package it.polimi.distsys.jmscluster.utils;
 
-import javax.jms.QueueConnectionFactory;
+import java.net.ConnectException;
 
+import javax.jms.QueueConnectionFactory;
+import javax.jms.TopicConnectionFactory;
+import javax.naming.NamingException;
+
+import org.objectweb.joram.client.jms.ConnectionFactory;
 import org.objectweb.joram.client.jms.Queue;
+import org.objectweb.joram.client.jms.Topic;
+import org.objectweb.joram.client.jms.admin.AdminException;
 import org.objectweb.joram.client.jms.admin.AdminModule;
 import org.objectweb.joram.client.jms.admin.User;
 import org.objectweb.joram.client.jms.tcp.TcpConnectionFactory;
 
 public class LocalAdmin {
-	public static void main(String[] args) throws Exception {
+	
+	public void createJobsQueue(String host, int port)
+			throws AdminException, ConnectException, NamingException {
 		QueueConnectionFactory qcf = TcpConnectionFactory.create("localhost", 16010);
 		
 		AdminModule.connect(qcf, "root", "root");
@@ -25,6 +34,33 @@ public class LocalAdmin {
 		jndiCtx.bind("jobsQueue", jobsQueue);
 		jndiCtx.close();
 
+		AdminModule.disconnect();		
+	}
+	
+	public void createCoordinationTopic(String host, int port)
+			throws AdminException, ConnectException, NamingException {
+		TopicConnectionFactory tcf = TcpConnectionFactory.create(host, port);
+		
+//		AdminModule.connect(cf, "root", "root");
+		
+		User.create("anonymous", "anonymous");
+
+		Topic coordinationTopic = Topic.create("coordinationTopic");
+		coordinationTopic.setFreeReading();
+		coordinationTopic.setFreeWriting();
+		
+		javax.naming.Context jndiCtx = new javax.naming.InitialContext();
+
+		jndiCtx.bind("tcf", tcf);
+		jndiCtx.bind("coordinationTopic", coordinationTopic);
+		jndiCtx.close();
+
 		AdminModule.disconnect();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		LocalAdmin admin = new LocalAdmin();
+		admin.createJobsQueue("localhost", 16010);
+		admin.createCoordinationTopic("localhost", 16010);
 	}
 }
