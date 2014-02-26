ba60782cfc49242dc0db947a09aac679e447caca
Marcello Pogliani <pogliamarci@hotmail.it>
2014-02-26 10:48:16 +0100

    [jmscluster] Scripts + remove debug prints

--- jmscluster/client.sh
+++ jmscluster/client.sh
@@ -2,5 +2,13 @@
 
 JARFILE=jmscluster-1.0.jar
 
-java -cp $JARFILE:$JORAM_HOME/ship/bundle/* it.polimi.distsys.jmscluster.client.Client
+
+# Verify if JORAM_HOME is well defined
+if [ ! -r "$JORAM_HOME"/samples/bin/clean.sh ]; then
+  echo "The JORAM_HOME environment variable is not defined correctly"
+  echo "This environment variable is needed to run this program"
+  exit 1
+fi
+
+exec "${JAVA_HOME}"/bin/java -cp $JARFILE:$JORAM_HOME/ship/bundle/* it.polimi.distsys.jmscluster.client.Client
 
--- jmscluster/localAdmin.sh
+++ jmscluster/localAdmin.sh
@@ -2,8 +2,22 @@
 
 JARFILE=jmscluster-1.0.jar
 
-host=192.168.60.10
+host=localhost
 port=16010
 
-java -cp $JARFILE:$JORAM_HOME/ship/bundle/* it.polimi.distsys.jmscluster.utils.LocalAdmin $host $port
+if [ "$1" != "" ]; then
+	host=$1
+fi
 
+if [ "$1" != "" ]; then
+	host=$1
+fi
+
+# Verify if JORAM_HOME is well defined
+if [ ! -r "$JORAM_HOME"/samples/bin/clean.sh ]; then
+  echo "The JORAM_HOME environment variable is not defined correctly"
+  echo "This environment variable is needed to run this program"
+  exit 1
+fi
+
+exec "${JAVA_HOME}"/bin/java -cp $JARFILE:$JORAM_HOME/ship/bundle/* it.polimi.distsys.jmscluster.utils.LocalAdmin $host $port
